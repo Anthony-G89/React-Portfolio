bd0521a70e006e98a9be6141c9fbe42db625fcb5
Anthony Guerrero <adguerrero1935@gmail.com>
2021-02-23 14:44:24 -0500

    Working on my first Component which is my NavBar and styling NavBar

---
+++ src/components/NavBar/index.js
@@ -0,0 +1,24 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+import "./style.css";
+
+
+
+function NavBar () {
+    return (
+        <div>
+            <header>
+                <nav>
+                    <h1 className="logo">A.G.</h1>
+                    <ul className="nav-links">
+                        <li><a href="#">Work</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Connect</a></li>
+                    </ul>
+                </nav>
+            </header>
+        </div>
+    )
+};
+
+export default NavBar;
