d9e90ed8037a4ea590e8148f0d7a2c36bfb695c3
Anthony Guerrero <adguerrero1935@gmail.com>
2021-02-23 17:10:31 -0500

    Working on my landing page and still working on it

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,15 +1,25 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import React from "react";
+import pictureOFMe from "../../images/Profile Pic 2020 (3).jpg";
 import "./style.css";
 
 
-function LandingPage () {
-return (
-    <div>
-        <section className="home">
-          <h1 className="name">Anthony Guerrero</h1>
-        </section>
-    </div>
-)
+function LandingPage() {
+    return (
+        <div>
+            <section className="home">
+                <div className="row">
+                    <div className="col-lg-6">
+                        <img src={pictureOFMe} className=" imgOfMe" alt="Image of me" />
+                        <h3 className="">Hello! My name is <br /><span className="name">Anthony Guerrero</span></h3>
+                    </div>
+                    <div className="col-lg-6">
+                        <h2>I'm a front-end developer who codes with React.js</h2>
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
 };
 
 export default LandingPage;
