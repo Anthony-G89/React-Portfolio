33287048036855b1bf420c2b2a8b1e3f77cc2f90
Anthony Guerrero <adguerrero1935@gmail.com>
2021-02-23 16:21:19 -0500

    Working on the landingPage and styling section

---
+++ src/components/Home/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+
+
+function LandingPage () {
+return (
+    <div>
+        <section className="home">
+          <h1 className="name">Anthony Guerrero</h1>
+        </section>
+    </div>
+)
+};
+
+export default LandingPage;
