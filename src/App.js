03657d7f65e0404277d0dc4067b929ac32da8ade
Anthony Guerrero <adguerrero1935@gmail.com>
2021-02-24 17:05:11 -0500

    Added a function for when user hover over View my work btn arrow will rotate 90deg

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Home from "./components/Home";
 import  Projects  from "./components/Projects";
 import './App.css';
 
+
+
+
 function App() {
   return (
     <div className="applicationContainer">
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,7 +1,25 @@
+/* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable jsx-a11y/img-redundant-alt */
 import React from "react";
 import pictureOFMe from "../../images/Profile Pic 2020 (3).jpg";
 import "./style.css";
+
+
+var workBtn = document.querySelector(".workBtn");
+var arrowBtn = document.querySelector(".arrowBtn");
+
+
+workBtn.addEventListener("mouseenter", e => {
+   arrowBtn.style.transform = "rotate(90deg)"
+});
+
+
+workBtn.addEventListener("mouseleave", e => {
+    arrowBtn.style.transform = "rotate(0deg)"
+ });
+ 
+
+
 
 
 function LandingPage() {
@@ -16,6 +34,7 @@ function LandingPage() {
                     </div>
                     <div className="col-lg-6 myTitleContainer">
                         <h2 className="myTitle">I'm a <span className="workTitle">Full Stack Developer</span>, who builds interactive websites that run across platforms & devices</h2>
+                        <button className="workBtn">View my work <img className="arrowBtn" src="https://img.icons8.com/windows/32/000000/arrow.png"/></button>
                     </div>
                 </div>
             </section>
