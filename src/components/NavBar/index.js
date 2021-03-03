166ac3a89201ae4ffe512438586831efff484f03
Anthony Guerrero <adguerrero1935@gmail.com>
2021-03-03 12:45:46 -0500

    Added function for nav-links to pop up when hamburger menu is clicked on

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -4,7 +4,7 @@ import "./style.css";
 
 
 
-function NavBar({ show, slideNav, close }) {
+function NavBar(props) {
 
 
 
@@ -13,17 +13,15 @@ function NavBar({ show, slideNav, close }) {
             <header>
                 <nav>
                     <h1 className="logo">A.G.</h1>
-
-                    <ul style={{
-                        transition: show ? "transform 0.5s ease-in" : "",
-                        transform: show ? "translateX(0%)" : "translateX(100%)"
-                    }} className="nav-links">
-                        <li onClick={close} className="nav-item"><a href="#project">Projects</a></li>
-                        <li onClick={close} className="nav-item"><a href="#">About</a></li>
-                        <li onClick={close} className="nav-item"><a href="#">Connect</a></li>
-                    </ul>
-
-                    <div onClick={() => slideNav(true)}  className="hamburger-menu">
+                    {props.show ?
+                        <ul className="nav-links"
+                            style={{ transform: props.show ? "translateX(0%)" : null }}>
+                            <li onClick={props.close} className="nav-item"><a href="#project">Projects</a></li>
+                            <li onClick={props.close} className="nav-item"><a href="#">About</a></li>
+                            <li onClick={props.close} className="nav-item"><a href="#">Connect</a></li>
+                        </ul>
+                     : <div onClick={() => props.slideNav(false)}></div> }
+                    <div onClick={() => props.slideNav(true)} className="hamburger-menu">
                         <div className="line line-1"></div>
                         <div className="line line-2"></div>
                         <div className="line line-3"></div>
