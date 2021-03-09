461af8767837fc314d142884b47c428b87d53e59
Anthony Guerrero <adguerrero1935@gmail.com>
2021-03-09 12:51:39 -0500

    Deleted files and fixed shadow text on aboutMe and Connect

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,7 +1,6 @@
 /* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable jsx-a11y/img-redundant-alt */
 import React from "react";
-// import Typewriter from 'typewriter-effect';
 import pictureOFMe from "../../images/Profile Pic 2020 (3).jpg";
 import "./style.css";
 
@@ -28,46 +27,14 @@ function handleMouseLeave() {
 
 function LandingPage() {
 
-
-// var title = document.querySelector(".workTitle");
-
-// var typewriter = new typewriter( "workTitle", {
-//     loop: true,
-//     delay:75
-// });
-
-// typewriter
-//   .pauseFor(2500)
-//   .typeString('A simple yet powerful native javascript')
-//   .pauseFor(300)
-//   .deleteChars(10)
-//   .typeString('<strong>JS</strong> plugin for a cool typewriter effect and ')
-//   .typeString('<strong>only <span style="color: #27ae60;">5kb</span> Gzipped!</strong>')
-//   .pauseFor(1000)
-//   .start();
-
-
-
     return (
         <div>
-            {/* < Typewriter onInit={(typewriter) => {
-    typewriter.typeString('Hello World!')
-      .callFunction(() => {
-        console.log('String typed out!');
-      })
-      .pauseFor(2500)
-      .deleteAll()
-      .callFunction(() => {
-        console.log('All strings were deleted');
-      })
-      .start();
-    }} />  */}
+      
             <section className="home">
                 <div className="row">
                     <div className="squareBackground"></div>
                     <div className="col-lg-6 pictureAndName">
                         <img src={pictureOFMe} className=" imgOfMe" alt="Image of me" />
-                        {/* <span className="mee">HELLO</span> */}
                         <h3 className="introParagraph">Hello! My name is <br /><span className="name">Anthony Guerrero</span></h3>
                     </div>
                     <div className="col-lg-6 myTitleContainer">
