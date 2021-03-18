7798632c720152a5f444a348714e08b9854f7f4f
Anthony Guerrero <adguerrero1935@gmail.com>
2021-03-18 15:12:18 -0400

    fixed background height for mobile

--- src/components/About-Me/index.js
+++ src/components/About-Me/index.js
@@ -17,7 +17,7 @@ function aboutMe({close}) {
 
                 <div className="row aboutMeRow">
                     <div className="col-lg-6 aboutMeSection">
-                        <p className="textAboutMe">I'm a driven Full Stack Developer who love to code and learn new Libraries and frameworks. I completed over 600 hours at UCF Coding Boot Camp. I'm Fast learner, hard worker, can adapt to any changes, and a team player who is proficient in an array of programming languages. On my free time, I like to go to the gym, play Fifa, and watch soccer or football.
+                        <p className="textAboutMe">I'm a driven Full Stack Developer who loves to code and learn new Libraries and frameworks. I completed over 600 hours at UCF Coding Boot Camp. I'm Fast learner, hard worker, can adapt to any changes, and a team player who is proficient in an array of programming languages. On my free time, I like to go to the gym, play Fifa, and watch soccer or football.
                         </p>
                         <a className="resumeBtn" href="https://drive.google.com/file/d/1kEFu3n0Qe2xRWVQt9aPIIwvmrFsKyTP6/view?usp=sharing" target="_blank" rel="noreferrer">My Resume
                         <img className="resumeIcon" src="https://img.icons8.com/office/40/000000/open-resume.png" />
