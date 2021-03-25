a705ce965cff7bf67bd27c5137f4672d539cb3dd
Anthony Guerrero <adguerrero1935@gmail.com>
2021-03-24 20:19:03 -0400

    Made changes to the hamburger menu on how it is display

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -8,6 +8,7 @@ function NavBar({ show, slideNav, close, open }) {
 
 
 
+
     return (
         <div>
             <header>
@@ -15,17 +16,18 @@ function NavBar({ show, slideNav, close, open }) {
                     <h1 className="logo">A.G.</h1>
 
                     <ul className="nav-links"
-                        style={{ transform: show ? "translateX(0%)" : null }}>
+                        style={{ transform: show ? "translateX(0%)" : "translateX(100%)" }}>
                         <li onClick={close} className="nav-item"><a className="navBar-Links" href="#projectSection">Projects</a></li>
                         <li onClick={close} className="nav-item"><a className="navBar-Links" href="#aboutMESection">About</a></li>
                         <li onClick={close} className="nav-item"><a className="navBar-Links" href="#connectSection">Connect</a></li>
                     </ul>
 
-                    <div onClick={open}  className="hamburger-menu">
+                    <div onClick={open} className="hamburger-menu">
                         <div className="line line-1"></div>
                         <div className="line line-2"></div>
                         <div className="line line-3"></div>
                     </div>
+
 
                 </nav>
             </header>
