0c255ae64607939085cf4a0ca67bd4dbce5edcbc
Anthony Guerrero <adguerrero1935@gmail.com>
2021-05-03 12:19:06 -0400

    Added a close icon for when the user clicks on the hamburger-menu, a close icon will appear and the hamburger-menu will disappear

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,5 +1,6 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
+import closeBtn from "../../images/closeIcon.png"
 import "./style.css";
 
 
@@ -7,9 +8,10 @@ import "./style.css";
 function NavBar({ show, slideNav, close, }) {
 
 
-
-
     return (
+
+
+
         <div>
             <header>
                 <nav>
@@ -17,16 +19,20 @@ function NavBar({ show, slideNav, close, }) {
 
                     <ul className="nav-links"
                         style={{ transform: show ? "translateX(0%)" : null }}>
-                        <li onClick={close} className="nav-item"><a className="navBar-Links" href="#projectSection">Projects</a></li>
-                        <li onClick={close} className="nav-item"><a className="navBar-Links" href="#aboutMESection">About</a></li>
-                        <li onClick={close} className="nav-item"><a className="navBar-Links" href="#connectSection">Connect</a></li>
+                        <li onClick={close} className={"nav-item"}><a className={"navBar-Links"} href="#projectSection">Projects</a></li>
+                        <li onClick={close} className={"nav-item"}><a className={"navBar-Links"} href="#aboutMESection">About</a></li>
+                        <li onClick={close} className={"nav-item"}><a className={"navBar-Links"} href="#connectSection">Connect</a></li>
                     </ul>
 
-                    <div onClick={() => slideNav(!show)} className="hamburger-menu">
+                    <img onClick={() => slideNav(!show)} className={"closeBtn"} alt={"close button"} src={closeBtn} style={{ display: !show ? "none" : null }} />
+
+                    <div onClick={() => slideNav(!show)} className={"hamburger-menu"}
+                        style={{ display: show ? "none" : null }} >
                         <div className={"line line-1"}></div>
                         <div className={"line line-2"}></div>
                         <div className={"line line-3"}></div>
                     </div>
+
 
 
                 </nav>
