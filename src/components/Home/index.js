f31b21ecdd21e9a33cddfb43d5a385174ef0c85e
Anthony Guerrero <adguerrero1935@gmail.com>
2021-06-22 17:25:54 -0400

    added more weight to the quote

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,26 +2,16 @@
 /* eslint-disable jsx-a11y/img-redundant-alt */
 import React from "react";
 import pictureOFMe from "../../images/Profile Pic 2020 (3).jpg";
+import babyYoda from "../../images/icons8-baby-yoda-144.png";
 // import Arrow from "../../images/icons8-up-64.png";
 import Typewriter from "typewriter-effect";
 import "./style.css";
 
 
-
-
-// window.addEventListener("scroll", () =>{
-//     const scrolled = window.scrollY
-//     // console.log(scrolled);
-// })
-
-// window.addEventListener("scroll", () => {
-//     const arrowBtn = document.querySelector(".upArrow");
-//     arrowBtn.classList.toggle("active", window.scrollY > 2100);
-
-
-
-
 function LandingPage({ close }) {
+
+
+
 
     return (
         <div>
@@ -31,8 +21,12 @@ function LandingPage({ close }) {
                 <div className="row">
                     <div className="squareBackground"></div>
                     <div className="col-lg-6 pictureAndName">
-                        
-                        <img src={pictureOFMe}   className=" imgOfMe" alt="Image of me" />
+
+                        <img src={pictureOFMe} className=" imgOfMe" alt="Image of me" />
+                        <div className={"overLay"}>
+                            <img className={"yoda"} src={babyYoda} alt={"Image of baby Yoda"}  />
+                            <p className={"groguQuotes"}>"He is strong with the force, but talent without training is nothing".</p>
+                        </div>
                         <h3 className="introParagraph">Hello! My name is <br /><span className="name">Anthony Guerrero</span></h3>
                     </div>
                     <div className="col-lg-6 myTitleContainer">
