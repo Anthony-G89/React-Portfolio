018e4b3b5699ddc8dbd64f3ae465ee98830114fa
Anthony Guerrero <adguerrero1935@gmail.com>
2021-07-01 14:13:07 -0400

    Fixed width on Modal when open for Tablets

--- src/components/About-Me/index.js
+++ src/components/About-Me/index.js
@@ -10,8 +10,6 @@ import "./style.css";
 
 function aboutMe({ close }) {
 
-
-
     return (
         <span id="aboutMESection">
 
@@ -20,10 +18,10 @@ function aboutMe({ close }) {
 
                 <div className="row aboutMeRow">
                     <div className="col-lg-6 aboutMeSection">
-                        <p className="textAboutMe">Hello young Padawan. Anthony is a driven Full Stack Developer who loves to code, learn new Libraries and frameworks. He has completed over 600 hours at UCF Coding Boot Camp - and continues to work and improve his skills daily. He is a hard worker and fast learner that can adapt to any changes. When he's not busy coding, he burns some energy at the gym or plays video games. Let's also not forget his favorites, soccer and football. Basically, he rocks and you would be lucky to work with him. May the Force be with you.
+                        <p className="textAboutMe animate" data-animate="animTextAboutMe 5s">Hello young Padawan. Anthony is a driven Full Stack Developer who loves to code, learn new Libraries and frameworks. He has completed over 600 hours at UCF Coding Boot Camp - and continues to work and improve his skills daily. He is a hard worker and fast learner that can adapt to any changes. When he's not busy coding, he burns some energy at the gym or plays video games. Let's also not forget his favorites, soccer and football. Basically, he rocks and you would be lucky to work with him. May the Force be with you.
                         </p>
                         <a className="resumeBtn" href={"https://drive.google.com/file/d/1O9zzyrVccH8DBHWH41Pmq8M_ouAhxKAS/view?usp=sharing"} target="_blank" >My Resume
-                            <img className="resumeIcon" src={"https://img.icons8.com/office/40/000000/open-resume.png"} alt={"Icon picture of a resume"} />
+                            <img className="resumeIcon animate" data-animate="animResumeBtn 5s" src={"https://img.icons8.com/office/40/000000/open-resume.png"} alt={"Icon picture of a resume"} />
                         </a>
 
                     </div>
